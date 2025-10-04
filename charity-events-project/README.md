05b090ab0f28326cc220eb98cd4efbc5e2d60ab3
Xinxin-Yang <3116249871@qq.com>
2025-10-04 23:42:05 +0800

    Final Revision 2
    I deleted some unnecessary files

--- charity-events-project/README.md
+++
@@ -1,6 +0,0 @@
-# Charity Events Website
-
-Project Overview
-Dynamic charity event management website, using Node.js + Express + MySQL + Vanilla JavaScript.
-
-Project structure
--- charity-events-project/api-server/routes/events.js
+++ charity-events-project/api-server/routes/events.js
@@ -2,62 +2,6 @@
  * 事件路由
  * 定义事件相关的API端点
  */
-/*
-const express = require('express');
-const router = express.Router();
-const eventController = require('../controllers/eventController');
-const validationMiddleware = require('../middleware/validation');
-const { ErrorHandler } = require('../middleware/errorHandler');
-
-// 获取所有活动
-router.get('/', 
-    ErrorHandler.asyncHandler(async (req, res) => {
-        await eventController.getAllEvents(req, res);
-    })
-);
-
-// 根据ID获取单个活动
-router.get('/:id', 
-    validationMiddleware.validateEventId,
-    ErrorHandler.asyncHandler(async (req, res) => {
-        await eventController.getEventById(req, res);
-    })
-);
-
-// 搜索活动 - 修复路径
-router.get('/search/query', 
-    validationMiddleware.validateSearchQuery,
-    ErrorHandler.asyncHandler(async (req, res) => {
-        await eventController.searchEvents(req, res);
-    })
-);
-
-// 获取即将到来的活动
-router.get('/upcoming/featured', 
-    ErrorHandler.asyncHandler(async (req, res) => {
-        await eventController.getUpcomingEvents(req, res);
-    })
-);
-
-// 获取热门活动
-router.get('/featured/popular', 
-    ErrorHandler.asyncHandler(async (req, res) => {
-        await eventController.getFeaturedEvents(req, res);
-    })
-);
-
-// 获取活动统计信息
-router.get('/stats/summary', 
-    ErrorHandler.asyncHandler(async (req, res) => {
-        await eventController.getEventStats(req, res);
-    })
-);
-
-module.exports = router;*/
-/**
- * 事件路由
- * 定义事件相关的API端点
- */
 
 const express = require('express');
 const router = express.Router();
